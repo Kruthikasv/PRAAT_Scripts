5ab682b6f56d590d58abd6662a9fb1e998c8ea78
Kruthika Vasisht <61304938+Kruthikasv@users.noreply.github.com>
2021-09-11 13:14:39 +0530

    Update Shimmer.py

--- Shimmer.py
+++ Shimmer.py
@@ -1,13 +1,13 @@
 #SHIMMER CALCULATION USING PYTHON
 
-Shimmer function in python
+#Shimmer function in python
 def shimmer(file_name=None):
     sound = parselmouth.Sound(file_name)
     pointprocess = call(sound, "To PointProcess (periodic, cc)", 75, 600)
     localshimmer = call(sound, pointprocess, "Get shimmer (local_dB)”, 0, 0, 0.0001, 0.02, 1.3, 1.6)
     return (util.format_output(localshimmer, 6) + “dB”)
 
-Shimmer_percentage function in python
+#Shimmer_percentage function in python
 def shimmer_percentage(file_name=None):
     sound = parselmouth.Sound(file_name)
     pointprocess = call(sound, "To PointProcess (periodic, cc)", 75, 600)
